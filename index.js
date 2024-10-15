49c72aec407b1bc5c350b4d9f318f660797a7502
Rodri <rodrigovizcarra9623@gmail.com>
2024-10-14 22:55:07 -0300

    Codigo inicial

---
+++ index.js
@@ -0,0 +1,25 @@
+let containerPokemon = document.querySelector(".containerPokemon");
+let btnContainer = document.querySelector(".btnContainer");
+let prevBtn, nextBtn;
+let inicialURL = "https://pokeapi.co/api/v2/pokemon/";
+let loader = document.getElementById("loader");
+
+const peticionApi = async (url) => {
+    
+};
+
+const obtenerDataPokemon = async (data, dataResponse) => {
+    
+};
+
+
+const pintarPokemones = (pokemon, dataResponse) => {
+    
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+})
+
+document.addEventListener("click", (e) => {
+    
+})
